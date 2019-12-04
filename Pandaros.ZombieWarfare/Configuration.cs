07a8b022244aebf399dd258eb8d263e8949ac742
Jim Burlison <JBurlison@Gmail.com>
2019-12-03 22:07:19 -0500

    initial commit

---
+++ Pandaros.ZombieWarfare/Configuration.cs
@@ -0,0 +1,43 @@
+﻿using Pandaros.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandaros.ZombieWarfare
+{
+    [ModLoader.ModManager]
+    internal class Configuration
+    {
+        public static CSModConfiguration ConfigurationBase { get; set; } = new CSModConfiguration(GameLoader.NAMESPACE);
+
+        public static bool TeleportPadsRequireMachinists
+        {
+            get => GetorDefault(nameof(TeleportPadsRequireMachinists), false);
+            private set => SetValue(nameof(TeleportPadsRequireMachinists), value);
+        }
+
+        public static bool HasSetting(string setting)
+        {
+            return Configuration.HasSetting(setting);
+        }
+
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterSelectedWorld, GameLoader.NAMESPACE + ".Configuration.AfterSelectedWorld")]
+        [ModLoader.ModCallbackDependsOn(GameLoader.NAMESPACE + ".AfterSelectedWorld")]
+        public static void AfterSelectedWorld()
+        {
+        }
+
+        public static T GetorDefault<T>(string key, T defaultVal)
+        {
+            return ConfigurationBase.GetorDefault(key, defaultVal);
+        }
+
+        public static void SetValue<T>(string key, T val)
+        {
+            ConfigurationBase.SetValue(key, val);
+        }
+    }
+}
---
+++ Pandaros.ZombieWarfare/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pandaros.ZombieWarfare")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pandaros.ZombieWarfare")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("edf7dd0f-8aa3-443c-a9f7-c3f100a86e55")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.1204.5609")]
+[assembly: AssemblyFileVersion("1.1.1204.7277")]
